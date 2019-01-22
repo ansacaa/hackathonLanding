33ec3939e9ceb3091e28da8d118354f1f8172b2f
Carlos2396 <carlosaam96@gmail.com>
2019-01-22 15:16:49 -0600

    Created laravel project, pulled in Spatie permissions library. Created users table seeder.

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'admin']);
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('secret')
+        ]);
+        $admin->assignRole('admin');
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => Hash::make('secret')
+        ]);
+    }
+}
