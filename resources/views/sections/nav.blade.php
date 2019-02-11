bf2910842af15fb82cc640bdecb8c6f3c2d1203d
Carlos2396 <carlosaam96@gmail.com>
2019-02-11 14:34:36 -0600

    Added MLH badge

--- resources/views/sections/nav.blade.php
+++ resources/views/sections/nav.blade.php
@@ -20,6 +20,7 @@
                 <li><a class="section-scroll" href="@if(Route::currentRouteName() == 'index'){{'#gallery'}}@else{{ url('/#gallery') }}@endif">Galería</a></li>
                 <li><a class="section-scroll" href="@if(Route::currentRouteName() == 'index'){{'#contacto'}}@else{{ url('/#contacto') }}@endif">Contacto</a></li>
                 <li><a class="section-scroll" href="{{ route('teams.create')}}">Inscripciones</a></li>
+                <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg" alt="Major League Hacking 2019 Hackathon Season" style="width:100%"></a>
             </ul>
         </div>
     </div>
