7d29461b93aac5498da742e4d469cb8756c427b0
Carlos2396 <carlosaam96@gmail.com>
2019-03-15 11:40:55 -0600

    Added Luxoft and Tsystems

--- resources/views/sections/sponsors.blade.php
+++ resources/views/sections/sponsors.blade.php
@@ -7,32 +7,31 @@
         </div>
         <div class="row multi-columns-row">
             <div class="row">
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="content-box">
-                        <div class="content-box-image">
-                            <a href="https://github.com">
-                                <img src="{{ asset('assets/images/github-logo.svg') }}" alt="Git Hub"/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="content-box">
-                        <div class="content-box-image">
-                            <a href="https://www.golucid.co/">
-                                <img src="{{ asset('assets/images/lucidchart-logo-small.png') }}" alt="Lucid Software"/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="content-box">
                         <div class="content-box-image">
                             <a href="https://konfio.mx/">
                                 <img src="{{ asset('assets/images/KonfioMorado.png') }}" alt="Konfio"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="content-box">
+                        <div class="content-box-image">
+                            <a href="https://verato.com/">
+                                <img src="{{ asset('assets/images/tsystems.png') }}" alt="Verato"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="content-box">
+                        <div class="content-box-image">
+                            <a href="https://www.t-systems.com/mx/es">
+                                <img src="{{ asset('assets/images/VeratoLogo.jpeg') }}" alt="Verato"/>
                             </a>
                         </div>
                     </div>
@@ -43,8 +42,8 @@
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="content-box">
                         <div class="content-box-image">
-                            <a href="https://verato.com/">
-                                <img src="{{ asset('assets/images/VeratoLogo.jpeg') }}" alt="Verato"/>
+                            <a href="https://www.golucid.co/">
+                                <img src="{{ asset('assets/images/lucidchart-logo.png') }}" style="max-height: 70px;" alt="Lucid Software"/>
                             </a>
                         </div>
                     </div>
@@ -53,18 +52,29 @@
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="content-box">
                         <div class="content-box-image">
-                            <a href="https://www.bloomberg.com/">
-                                <img src="{{ asset('assets/images/bloomberg-logo.svg') }}" alt="Git Hub"/>
+                            <a href="https://career.luxoft.com/">
+                                <img src="{{ asset('assets/images/luxoftlogos.png') }}" style="max-height: 70px;" alt="Luxoft"/>
                             </a>
                         </div>
                     </div>
                 </div>
-    
+            </div>
+            <div class="row" style="margin-top:50px;">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="content-box">
+                        <div class="content-box-image">
+                            <a href="https://github.com">
+                                <img src="{{ asset('assets/images/github-logo.svg') }}" style="max-height: 50px;" alt="Git Hub"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="col-md-4 col-sm-6 col-xs-12">
                     <div class="content-box">
                         <div class="content-box-image">
                             <a href="https://www.stickermule.com/uses/laptop-stickers?utm_source=sponsorship&utm_campaign=mlh-sponsorship&utm_medium=referral?utm_source=Direct">
-                                <img src="{{ asset('assets/images/sticker-mule-logo.svg') }}" alt="Bloomberg"/>
+                                <img src="{{ asset('assets/images/sticker-mule-logo.svg') }}" style="max-height: 50px;" alt="Sticker Mule"/>
                             </a>
                         </div>
                     </div>
